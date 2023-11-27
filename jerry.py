2899cb1314be0204446bd8a643616fc16fe38bd2
Yascoded <152174270+Yascoded@users.noreply.github.com>
2023-11-27 12:57:12 +0100

    Add files via upload
    Web-Based Beauty salon booking system using blockchain Tech

---
+++ jerry.py
@@ -0,0 +1,30 @@
+
+from app import app
+import os
+
+# sinple block of code to check if app is running in a virtual environment
+running_in_virtualemv = "VIRTUAL_ENV " in os.environ
+
+in_venv = bool(os.environ.get("VIRTUAL_ENV"))
+in_venv2 = bool(os.getenv("VIRTUAL_ENV"))
+
+
+def in_virtual_env_method_one():
+    print('[RUNNING IN A VIRTUAL ENVIRONMENT (method 1)] : ',in_venv )
+
+def in_virtual_env_method_two():
+    print( '[RUNNING IN A VIRTUAL ENVIRONMENT (method 2)] : ', in_venv2)
+
+
+
+in_virtual_env_method_one()
+in_virtual_env_method_two()
+# end of function block
+
+# print (in_virtual_env())
+
+
+
+
+if __name__ == '__main__':
+	app.run(host = "localhost", threaded = True, port=1225, debug=True) 
